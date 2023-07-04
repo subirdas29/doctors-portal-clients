4806fb7321762c54cb3ae14aaa1850081b28b375
subirdas29 <dasjoy104@gmail.com>
2023-07-04 23:06:02 +0600

    new added

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className='max-w-[1440px] mx-auto'>
     <RouterProvider router={router}></RouterProvider>
-    <Toaster />
+    <Toaster />d
     </div>
   );
 }
